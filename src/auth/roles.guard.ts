98e5c21603c070cb3652450e66dfd1979c3da7d8
ST10079108 <st10079108@vcconnect.edu.za>
2026-08-05 16:41:07 +0200

    feat: wip roles, decorter, enum, guard

---
+++ src/auth/roles.guard.ts
@@ -0,0 +1,46 @@
+// import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+// import { Reflector } from '@nestjs/core';
+// import { Role } from './roles.enum';
+// import { ROLES_KEY } from './roles.decorator';
+
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+// @Injectable()
+// export class RolesGuard implements CanActivate {
+//   constructor(private reflector: Reflector) {}
+
+//   canActivate(context: ExecutionContext): boolean {
+//     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+//       context.getHandler(),
+//       context.getClass(),
+//     ]);
+//     if (!requiredRoles) {
+//       return true;
+//     }
+//     const { user } = context.switchToHttp().getRequest();
+//     if (!user) {
+//       return false;
+//     }
+//     return requiredRoles.includes(user.role);
+//   }
+//
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  matchRoles(roles: string[], userRole: string) {
+    return roles.some((role) => role === userRole);
+  }
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    if (!roles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    return this.matchRoles(roles, user.role);
+  }
+}
